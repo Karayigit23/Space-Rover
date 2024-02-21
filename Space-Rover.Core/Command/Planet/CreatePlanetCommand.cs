d83cec3fcef9cda4e0cb05a22075b82b8dfa1fe4
Karayigit23 <karayigit833@gmail.com>
2024-02-21 09:49:13 +0300

    Created CreatePlanetCommand.cs

---
+++ Space-Rover.Core/Command/Planet/CreatePlanetCommand.cs
@@ -0,0 +1,31 @@
+using MediatR;
+using Space_Rover.Core.InterFaces;
+
+namespace Space_Rover.Core.Command.Planet;
+
+public class CreatePlanetCommand:IRequest<Entity.Planet>
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+}
+
+public class CreatePlanetCommandHandler : IRequestHandler<CreatePlanetCommand, Entity.Planet>
+{
+    private readonly IPlanetRepository _planetRepository;
+
+    public CreatePlanetCommandHandler(IPlanetRepository planetRepository)
+    {
+        _planetRepository = planetRepository;
+    }
+    public async Task<Entity.Planet> Handle(CreatePlanetCommand request, CancellationToken cancellationToken)
+    {
+        //buraya boş girildiğinde bir hata atmak için exaption koy
+        var planet = new Entity.Planet
+        {
+            Height = request.Height,
+            Width = request.Width
+        };
+        await _planetRepository.AddPlanet(planet);
+        return planet;
+    }
+}

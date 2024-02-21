97df27213bab6213e7f17deea4de6ed17abf5b1d
Karayigit23 <karayigit833@gmail.com>
2024-02-21 09:33:19 +0300

    Create rover Controller

---
+++ Space-Rover.Core/Query/Rover/GetByIdRover.cs
@@ -0,0 +1,35 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Space_Rover.Core.InterFaces;
+
+namespace Space_Rover.Core.Query.Rover;
+
+public class GetByIdRover:IRequest<Entity.Rover>
+{
+    public int Id { get; set; }
+}
+public class GetByIdRoverHandler : IRequestHandler<GetByIdRover, Entity.Rover>
+{
+    private readonly IRoverRepository _roverRepository;
+    private readonly ILogger<GetByIdRoverHandler> _logger;
+    public GetByIdRoverHandler(IRoverRepository roverRepository,ILogger<GetByIdRoverHandler>
+        logger)
+    {
+        _roverRepository = roverRepository;
+        _logger = logger;
+    }
+    public async Task<Entity.Rover> Handle(GetByIdRover request, CancellationToken cancellationToken)
+    {
+  
+        _logger.LogInformation(message:$"{request.Id} User came");
+        var result = await _roverRepository.GetByIdRover(request.Id);
+        /*if (result==null)
+        {
+   
+            throw new NotFoundException($"user not found userId: {request.Id}");   //buraya exaption handler konulabilir
+        }*/
+
+        return result;
+    }
+}
+
--- Space-Rover/Controllers/Rover.cs
+++ Space-Rover/Controllers/Rover.cs
@@ -1,5 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Space_Rover.Core.Command.Rover;
+using Space_Rover.Core.Query.Rover;
 
 namespace Space_Rover.Controllers;
 [ApiController]
@@ -15,10 +17,38 @@ public class Rover : ControllerBase
    
    [HttpGet]
    
-   public async Task<List<Rover>> GetAllRover()
+   public async Task<List<Core.Entity.Rover>> GetAllRover()
    {
-      var Rover = await _mediator.Send(new GetAllUserQuery());
+      var Rover = await _mediator.Send(new GetAllRoverQuery());
       return Rover;
+   }
+   
+   [HttpGet("{roverId}")]
+   public async Task<Core.Entity.Rover> GetByIdRover(int Id)
+   {
+      var rover= await _mediator.Send(request: new GetByIdRover { Id = Id });
+      return rover;
+   }
+
+   
+   [HttpPost]
+   public async Task AddRover ([FromBody] CreateRoverCommand rover)
+   {
+      await _mediator.Send(rover);
+   }
+
+   [HttpPut("{Id}")]
+   public async Task UpdateRover(int Id, [FromBody] UpdateRoverCommand rover)
+   {
+      rover.ıd = Id;
+      await _mediator.Send(rover);
+   }
+
+   [HttpDelete("{Id}")]
+   public async Task DeleteRover(int Id)
+   {
+      await _mediator.Send(new DeleteRoverCommand { Id = Id });
+            
    }
 
 }

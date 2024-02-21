9a7e26d13d72ab3c188bfe6f9efcd929da8fe304
Karayigit23 <karayigit833@gmail.com>
2024-02-21 09:55:01 +0300

    Created UpdatePlanetCommand.cs

---
+++ Space-Rover.Core/Command/Planet/UpdatePlanetCommand.cs
@@ -0,0 +1,31 @@
+using MediatR;
+using Space_Rover.Core.InterFaces;
+
+namespace Space_Rover.Core.Command.Planet;
+
+public class UpdatePlanetCommand:IRequest<Entity.Planet>
+{
+    public int Id { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+}
+
+public class UpdatePlanetCommandHandler : IRequestHandler<UpdatePlanetCommand, Entity.Planet>
+{
+    private readonly IPlanetRepository _planetRepository;
+
+    public UpdatePlanetCommandHandler(IPlanetRepository planetRepository)
+    {
+        _planetRepository = planetRepository;
+    }
+    public async Task<Entity.Planet> Handle(UpdatePlanetCommand request, CancellationToken cancellationToken)
+    {
+        var planet = await _planetRepository.GetByIdPlanet(request.Id);
+
+        planet.Height = request.Height;
+        planet.Width = request.Width;
+
+        await _planetRepository.UpdatePlanet(planet);
+        return planet;
+    }
+}
